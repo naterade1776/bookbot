fbc706161fad63210b1174e993abd0043b8b2ea8
naterade1776 <naterade@mailfence.com>
2024-11-13 22:14:59 +0000

    refactored to count characters. Still lots of comments.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import re
+#import os
 with open("books/frankie.txt") as txtf:
-	fcontent = txtf.read()
+	fcontent = list(txtf.read())
+	characterDict = {}
+	print("Processing...")
+	for character in fcontent:
+#		os.system("clear")
+#		print("Processing...")
+		if character not in characterDict:
+			characterDict[character] = 1
+		else:
+			characterDict[character] = characterDict[character] + 1
+#	print(fcontent)
 	count = 0
-	fwords = fcontent.split()
-	for word in fwords:
-		count = count+1
-	print(fwords)
-	print(f"WORDS IN FILE: {count}")
+#	fwords = fcontent.split()
+#	for word in fwords:
+#		count = count+1
+	print(characterDict)
+#	print(f"WORDS IN FILE: {count}")
