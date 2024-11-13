04da9a64722dd01a7959736872c4ef1685a0b00e
naterade1776 <naterade@mailfence.com>
2024-11-13 22:48:47 +0000

    refined results and output. Counts words again.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import re
-#import os
-with open("books/frankie.txt") as txtf:
-	fcontent = list(txtf.read())
+
+
+
+bookfile = "books/frankenstein.txt"
+with open(bookfile) as txtfile:
+	fcontent = txtfile.read()
+	characters = list(fcontent)
+	words = fcontent.split()
+	wordCount = 0
+	for word in words:
+		wordCount = wordCount + 1
 	characterDict = {}
-	print("Processing...")
-	for character in fcontent:
-#		os.system("clear")
-#		print("Processing...")
-		if character not in characterDict:
-			characterDict[character] = 1
-		else:
-			characterDict[character] = characterDict[character] + 1
-#	print(fcontent)
-	count = 0
-#	fwords = fcontent.split()
-#	for word in fwords:
-#		count = count+1
-	print(characterDict)
-#	print(f"WORDS IN FILE: {count}")
+	letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	for character in characters:
+		if character in letters:
+			character = character.lower()
+			if character not in characterDict:
+				characterDict[character] = 1
+			else:
+				characterDict[character] = characterDict[character] + 1
+print("\n--- Begin report of books/frankenstein.txt ---\n")
+print(f"\n{wordCount} words were found in {bookfile}\n")
+
+for character in characterDict:
+	print(f"Character {character} was found {characterDict[character]} times")
+print("\n--- End report ---\n")
